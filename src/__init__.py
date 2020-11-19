4d46223b3075dc84225bd1adaac06e7da218b197
EBO9002 <algorithm01@protonmail.com>
2020-11-19 18:42:51 +0100

    Initial commit

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+version = "0.0"
+
+
+
+from .config import proxy_set
+from .config import cmd_templates
+
+from .loc import check
+from .loc import search 
+
+from .net import phrack_paraser
+from .net import get_text
+from .net import get_content_dir
+from .net import get_index
+
+
+
+
+
